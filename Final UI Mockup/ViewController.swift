c9ca3bbc9c4ad114285142e9307b5a0363a75f37
Brad Gowland <brg301@nyu.edu>
2017-12-10 20:14:58 -0500

    first working draft
    just needs more real patterns (motorik is placeholding for 10 of them).
    
    user can pick patterns or tap, change tempo and velocity.
    
    could add record feature and improve UI if we have time.

--- Final UI Mockup/ViewController.swift
+++ Final UI Mockup/ViewController.swift
@@ -11,103 +11,322 @@ import CoreMIDI
 
 class ViewController: UIViewController, NetServiceBrowserDelegate {
 
-    var tempoVal: Float = 120
-    var velocityVal: Float = 128
+    var tempoVal: Double = 0.25
+    var velocityVal: Float = 127
+    var timer: Timer? = nil
+    var mode = -1
+    var playing: Int = -1
+    var pattern: Int = 0
+    var step: Int = 0
+    
+    var beats: [[[Int]]] = [
+       // 0 - motorik
+       [[1,0,1,0,0,0,1,0,1,0,1,0,0,0,1,0],
+        [0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0],
+        [1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]],
+       // 1 - ramones
+       [[1,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0],
+        [0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0],
+        [1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]],
+       // 2 - fill 1
+       [[1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0],
+        [0,0,0,0,1,0,0,0,0,0,1,1,1,1,1,1],
+        [1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]],
+       // 3 - fill 2
+       [[1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0],
+        [1,1,0,0,1,1,0,0,1,1,0,0,0,0,0,0],
+        [1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0],
+        [0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,1,1,0,0,0,0,1,1,1,1]],
+       // 4 - metal fill
+       [[0,0,1,1,0,0,1,1,0,0,1,1,0,0,0,0],
+        [1,1,0,0,0,0,0,0,0,0,0,0,1,0,0,0],
+        [1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0],
+        [0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0]],
+       // 5 - REPLACE THIS
+        [[1,0,1,0,0,0,1,0,1,0,1,0,0,0,1,0],
+         [0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0],
+         [1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0],
+         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]],
+        // 6 - REPLACE THIS
+        [[1,0,1,0,0,0,1,0,1,0,1,0,0,0,1,0],
+         [0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0],
+         [1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0],
+         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]],
+        // 7 - REPLACE THIS
+        [[1,0,1,0,0,0,1,0,1,0,1,0,0,0,1,0],
+         [0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0],
+         [1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0],
+         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]],
+        // 8 - REPLACE THIS
+        [[1,0,1,0,0,0,1,0,1,0,1,0,0,0,1,0],
+         [0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0],
+         [1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0],
+         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]],
+        // 9 - REPLACE THIS
+        [[1,0,1,0,0,0,1,0,1,0,1,0,0,0,1,0],
+         [0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0],
+         [1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0],
+         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]],
+        // 10 - REPLACE THIS
+        [[1,0,1,0,0,0,1,0,1,0,1,0,0,0,1,0],
+         [0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0],
+         [1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0],
+         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]],
+        // 11 - REPLACE THIS
+        [[1,0,1,0,0,0,1,0,1,0,1,0,0,0,1,0],
+         [0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0],
+         [1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0],
+         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]],
+        // 12 - REPLACE THIS
+        [[1,0,1,0,0,0,1,0,1,0,1,0,0,0,1,0],
+         [0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0],
+         [1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0],
+         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]],
+        // 13 - REPLACE THIS
+        [[1,0,1,0,0,0,1,0,1,0,1,0,0,0,1,0],
+         [0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0],
+         [1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0],
+         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]],
+        // 14 - REPLACE THIS
+        [[1,0,1,0,0,0,1,0,1,0,1,0,0,0,1,0],
+         [0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0],
+         [1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0],
+         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]],
+        // 15 - REPLACE THIS
+        [[1,0,1,0,0,0,1,0,1,0,1,0,0,0,1,0],
+         [0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0],
+         [1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0],
+         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
+   ]
+
+    @IBOutlet weak var modeLabel: UILabel!
+    
+    @IBAction func toggleMode(_ sender: Any) {
+        self.mode = self.mode * -1
+        if (mode == -1) {
+            self.modeLabel.text = "Tap"
+        } else {
+            self.modeLabel.text = "1"
+        }
+    }
+    
+    @IBAction func togglePlay(_ sender: Any) {
+        self.playing = self.playing * -1
+        if (playing == 1) {
+            timer = Timer.scheduledTimer(withTimeInterval: self.tempoVal, repeats: true) { timer in
+                if (self.beats[self.pattern][0][self.step] == 1) {
+                    self.sendMessage(0x90, 0x2a, UInt8(self.velocityVal))
+                    self.sendMessage(0x80, 0x2a, 0x7f)
+                }
+                if (self.beats[self.pattern][1][self.step] == 1) {
+                    self.sendMessage(0x90, 0x26, UInt8(self.velocityVal))
+                    self.sendMessage(0x80, 0x26, 0x7f)
+                }
+                if (self.beats[self.pattern][2][self.step] == 1) {
+                    self.sendMessage(0x90, 0x24, UInt8(self.velocityVal))
+                    self.sendMessage(0x80, 0x24, 0x7f)
+                }
+                // TODO: add the rest of the drums here
+                self.step = (self.step + 1) % 16
+            }
+            timer?.fire()
+        } else {
+            timer?.invalidate()
+            self.step = 0
+        }
+    }
     
     @IBOutlet var tempo: UISlider!
     
     @IBAction func tempoChange(_ sender: Any) {
-        tempoVal = floor(tempo.value * 220) + 30
+        tempoVal = Double(15 / ((tempo.value * 220) + 30))
     }
     
     @IBOutlet var velocity: UISlider!
     
     @IBAction func velocityChange(_ sender: Any) {
-        velocityVal = floor(velocity.value * 128)
+        velocityVal = floor(velocity.value * 127)
         print(velocityVal)
     }
     
     @IBAction func kick(_ sender: Any) {
-        sendMessage(0x90, 0x24, UInt8(velocityVal))
-        sendMessage(0x80, 0x24, 0x7f)
+        if (self.mode == -1) {
+            sendMessage(0x90, 0x24, UInt8(velocityVal))
+            sendMessage(0x80, 0x24, 0x7f)
+        } else {
+            self.pattern = 0
+            self.modeLabel.text = String(self.pattern + 1)
+        }
     }
     
     @IBAction func rim(_ sender: Any) {
-        sendMessage(0x90, 0x25, UInt8(velocityVal))
-        sendMessage(0x80, 0x25, 0x7f)
+        if (self.mode == -1) {
+            sendMessage(0x90, 0x25, UInt8(velocityVal))
+            sendMessage(0x80, 0x25, 0x7f)
+        } else {
+            self.pattern = 1
+            self.modeLabel.text = String(self.pattern + 1)
+        }
     }
     
     @IBAction func snare(_ sender: Any) {
-        sendMessage(0x90, 0x26, UInt8(velocityVal))
-        sendMessage(0x80, 0x26, 0x7f)
+        if (self.mode == -1) {
+            sendMessage(0x90, 0x26, UInt8(velocityVal))
+            sendMessage(0x80, 0x26, 0x7f)
+        } else {
+            self.pattern = 2
+            self.modeLabel.text = String(self.pattern + 1)
+        }
     }
     
     @IBAction func clap(_ sender: Any) {
-        sendMessage(0x90, 0x27, UInt8(velocityVal))
-        sendMessage(0x80, 0x27, 0x7f)
+        if (self.mode == -1) {
+            sendMessage(0x90, 0x27, UInt8(velocityVal))
+            sendMessage(0x80, 0x27, 0x7f)
+        } else {
+            self.pattern = 3
+            self.modeLabel.text = String(self.pattern + 1)
+        }
     }
     
     @IBAction func hihat(_ sender: Any) {
-        sendMessage(0x90, 0x2a, UInt8(velocityVal))
-        sendMessage(0x80, 0x2a, 0x7f)
+        if (self.mode == -1) {
+            sendMessage(0x90, 0x2a, UInt8(velocityVal))
+            sendMessage(0x80, 0x2a, 0x7f)
+        } else {
+            self.pattern = 4
+            self.modeLabel.text = String(self.pattern + 1)
+        }
     }
     
     @IBAction func hihatOpen(_ sender: Any) {
-        sendMessage(0x90, 0x2e, UInt8(velocityVal))
-        sendMessage(0x80, 0x2e, 0x7f)
+        if (self.mode == -1) {
+            sendMessage(0x90, 0x2e, UInt8(velocityVal))
+            sendMessage(0x80, 0x2e, 0x7f)
+        } else {
+            self.pattern = 5
+            self.modeLabel.text = String(self.pattern + 1)
+        }
     }
     
     @IBAction func tomLo(_ sender: Any) {
-        sendMessage(0x90, 0x2d, UInt8(velocityVal))
-        sendMessage(0x80, 0x2d, 0x7f)
+        if (self.mode == -1) {
+            sendMessage(0x90, 0x2d, UInt8(velocityVal))
+            sendMessage(0x80, 0x2d, 0x7f)
+        } else {
+            self.pattern = 6
+            self.modeLabel.text = String(self.pattern + 1)
+        }
     }
     
     @IBAction func tomMid(_ sender: Any) {
-        sendMessage(0x90, 0x2f, UInt8(velocityVal))
-        sendMessage(0x80, 0x2f, 0x7f)
+        if (self.mode == -1) {
+            sendMessage(0x90, 0x2f, UInt8(velocityVal))
+            sendMessage(0x80, 0x2f, 0x7f)
+        } else {
+            self.pattern = 7
+            self.modeLabel.text = String(self.pattern + 1)
+        }
     }
     
     @IBAction func tomHi(_ sender: Any) {
-        sendMessage(0x90, 0x30, UInt8(velocityVal))
-        sendMessage(0x80, 0x30, 0x7f)
+        if (self.mode == -1) {
+            sendMessage(0x90, 0x30, UInt8(velocityVal))
+            sendMessage(0x80, 0x30, 0x7f)
+        } else {
+            self.pattern = 8
+            self.modeLabel.text = String(self.pattern + 1)
+        }
     }
     
     @IBAction func cymbal(_ sender: Any) {
-        sendMessage(0x90, 0x31, UInt8(velocityVal))
-        sendMessage(0x80, 0x31, 0x7f)
+        if (self.mode == -1) {
+            sendMessage(0x90, 0x31, UInt8(velocityVal))
+            sendMessage(0x80, 0x31, 0x7f)
+        } else {
+            self.pattern = 9
+            self.modeLabel.text = String(self.pattern + 1)
+        }
     }
     
     @IBAction func cowbell(_ sender: Any) {
-        sendMessage(0x90, 0x38, UInt8(velocityVal))
-        sendMessage(0x80, 0x38, 0x7f)
+        if (self.mode == -1) {
+            sendMessage(0x90, 0x38, UInt8(velocityVal))
+            sendMessage(0x80, 0x38, 0x7f)
+        } else {
+            self.pattern = 10
+            self.modeLabel.text = String(self.pattern + 1)
+        }
     }
     
     @IBAction func congaLo(_ sender: Any) {
-        sendMessage(0x90, 0x40, UInt8(velocityVal))
-        sendMessage(0x80, 0x40, 0x7f)
+        if (self.mode == -1) {
+            sendMessage(0x90, 0x40, UInt8(velocityVal))
+            sendMessage(0x80, 0x40, 0x7f)
+        } else {
+            self.pattern = 11
+            self.modeLabel.text = String(self.pattern + 1)
+        }
     }
     
     @IBAction func congaMid(_ sender: Any) {
-        sendMessage(0x90, 0x3f, UInt8(velocityVal))
-        sendMessage(0x80, 0x3f, 0x7f)
+        if (self.mode == -1) {
+            sendMessage(0x90, 0x3f, UInt8(velocityVal))
+            sendMessage(0x80, 0x3f, 0x7f)
+        } else {
+            self.pattern = 12
+            self.modeLabel.text = String(self.pattern + 1)
+        }
     }
     
     @IBAction func congaHi(_ sender: Any) {
-        sendMessage(0x90, 0x3e, UInt8(velocityVal))
-        sendMessage(0x80, 0x3e, 0x7f)
+        if (self.mode == -1) {
+            sendMessage(0x90, 0x3e, UInt8(velocityVal))
+            sendMessage(0x80, 0x3e, 0x7f)
+        } else {
+            self.pattern = 13
+            self.modeLabel.text = String(self.pattern + 1)
+        }
     }
     
     @IBAction func maraca(_ sender: Any) {
-        sendMessage(0x90, 0x46, UInt8(velocityVal))
-        sendMessage(0x80, 0x46, 0x7f)
+        if (self.mode == -1) {
+            sendMessage(0x90, 0x46, UInt8(velocityVal))
+            sendMessage(0x80, 0x46, 0x7f)
+        } else {
+            self.pattern = 14
+            self.modeLabel.text = String(self.pattern + 1)
+        }
     }
     
     @IBAction func clave(_ sender: Any) {
-        sendMessage(0x90, 0x4b, UInt8(velocityVal))
-        sendMessage(0x80, 0x4b, 0x7f)
+        if (self.mode == -1) {
+            sendMessage(0x90, 0x4b, UInt8(velocityVal))
+            sendMessage(0x80, 0x4b, 0x7f)
+        } else {
+            self.pattern = 15
+            self.modeLabel.text = String(self.pattern + 1)
+        }
     }
-    
-
     
     let browser = NetServiceBrowser()
     
